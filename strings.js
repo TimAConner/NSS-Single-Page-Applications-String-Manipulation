ede3b22c351147b2cba5db10ede65106d028f7af
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 15:06:54 -0600

    Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.

--- strings.js
+++ strings.js
@@ -1,12 +1,21 @@
+
+var testString = "";
+
 function activateButton(){
     document.getElementById("btn").addEventListener("click", function(event){
-        event.preventDefault()
-        
+        event.preventDefault();
+        testString = document.getElementById("textInput").value;
+        reversal(testString);
+        alphabits(testString);
+        palindrome(testString);
     });
 }
 
-function reversal() {
-    
+function reversal(text) {
+    let textArray = text.split("");
+    textArray = textArray.reverse();
+    textArray = textArray.join(" ");
+    print(textArray);
 }
 
 function alphabits() {
@@ -17,10 +26,14 @@ function palindrome() {
 
 }
 
+function print(text){
+    let paraNode = document.createElement("p");
+    let textNode = document.createTextNode(text);
+    paraNode.appendChild(textNode);
+    document.getElementById("output").appendChild(paraNode);
+}
 
-var testString = "";
+
 activateButton();
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+
 
