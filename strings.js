9ce26aeb714c29b8630803a9970a5393a8780241
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 14:48:59 -0600

    The text input should only accept letters.  No numbers.

--- strings.js
+++ strings.js
@@ -1,17 +1,26 @@
+function activateButton(){
+    document.getElementById("btn").addEventListener("click", function(event){
+        event.preventDefault()
+        
+    });
+}
+
 function reversal() {
     
-    }
-    
-    function alphabits() {
-    
-    }
-    
-    function palindrome() {
-    
-    }
-    
-    var testString = "";
-    reversal(testString);
-    alphabits(testString);
-    palindrome(testString);
+}
+
+function alphabits() {
+
+}
+
+function palindrome() {
+
+}
+
+
+var testString = "";
+activateButton();
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
 
